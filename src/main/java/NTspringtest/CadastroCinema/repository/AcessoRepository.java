af10d19bedc29d057a199eabdca578846f9be0f6
krebs <matheus.gonzaga.krebs@gmail.com>
2023-08-30 21:27:14 -0300

    Entidades feitas e rascunho repositorios

--- src/main/java/NTspringtest/CadastroCinema/repository/AcessoRepository.java
+++ src/main/java/NTspringtest/CadastroCinema/repository/AcessoRepository.java
@@ -1,6 +1,5 @@
 package NTspringtest.CadastroCinema.repository;
 import org.springframework.data.repository.CrudRepository;
-import package NTspringtest.CadastroCinema.entity.Acesso;
 public interface AcessoRepository {
 
 }
